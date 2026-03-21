96932b7ad26215b6b4b1ce36fe6441be22fc1d3d
aldus <drp@cms-lab.com>
2026-03-21 23:37:03 +0100

    More code cleanings.

--- core/sql/Pages.php
+++ core/sql/Pages.php
@@ -28,7 +28,7 @@ class Pages
 
     protected ?object $database = null;
     
-    public array $WBCE_all_pages = [];
+    public array $allPages = [];
 
     // Avoid using "new" for a new instance.
     protected function __construct()
@@ -52,19 +52,12 @@ class Pages
      *    @return    void    As the storage is called by reference.
      *
      */
-    public function page_tree(
+    public function getPageTree(
         int $root_id = 0,
         array &$page_storage = [],
         array $fields = ['page_id', 'page_title', 'menu_title', 'parent','position', 'visibility', 'admin_groups']
         ): void
     {
-        // global $WBCE_all_pages;
-        // $database = LEPTON_database::getInstance();
-
-        // [1.0.2]
-        // $bUserHasAdminRights = LEPTON_core::userHasAdminRights();
-        // [1.0.3]
-        // $aUserGroups = LEPTON_CORE::getValue("groups_id", "string", "session", ",");
 
         // [1.1] make sure that required fields are in list
         $aRequiredKeys = ['page_id', 'parent', 'visibility', 'admin_groups', 'link'];
@@ -79,47 +72,30 @@ class Pages
 
         $select_fields = "`".implode("`,`", $fields)."`";
 
-        $this->WBCE_all_pages = [];
+        $this->allPages = [];
         $this->execute_query(
             "SELECT ".$select_fields." FROM `".TABLE_PREFIX."pages` ORDER BY `parent`,`position`",
             true,
-            $this->WBCE_all_pages
+            $this->allPages
         );
 
         // [2.1]
-        foreach ($this->WBCE_all_pages as &$ref)
+        foreach ($this->allPages as &$ref)
         {
             $ref['admin_groups'] = explode(",", $ref['admin_groups']);
         }
         unset($ref);
 
-        // [2.2]
-/*
-        if ($bUserHasAdminRights == true)
-        {
-            foreach ($this->LEPTON_CORE_all_pages as &$ref)
-            {
-                $ref['userAllowed'] = true;
-            }
-        }
-        else
-        {
-            foreach ($this->LEPTON_CORE_all_pages as &$ref)
-            {
-                $ref['userAllowed'] = !empty(array_intersect($aUserGroups, $ref['admin_groups']));
-            }
-        }
-*/
         if (in_array("viewing_groups", $fields))
         {
-            foreach ($this->WBCE_all_pages as &$ref)
+            foreach ($this->allPages as &$ref)
             {
                 $ref['viewing_groups'] = explode(",", $ref['viewing_groups']);
             }
         }
         unset($ref);
 
-        $this->make_list($root_id, $page_storage);
+        $this->makeList($root_id, $page_storage);
     }
 
     /**
@@ -129,70 +105,22 @@ class Pages
      *    @param    array   $aRefArray Result-Storage. Call by reference!
      *
      */ 
-    protected function make_list(int $aNum, array &$aRefArray): void
+    protected function makeList(int $aNum, array &$aRefArray): void
     {
-        // global $WBCE_all_pages, $TEXT;
 
-        foreach($this->WBCE_all_pages as &$aTempPage)
+        foreach($this->allPages as &$aTempPage)
         {
 
             if ($aTempPage['parent'] > $aNum)
             {
-                // return;
                 break;
             }
             
 
             if ($aTempPage['parent'] == $aNum)
             {
-/*
-                switch ($aTempPage['visibility'])
-                {
-
-                    case 'public':
-                        $aTempPage['status_icon'] = "visible_16.png";
-                        $aTempPage['status_text'] = $TEXT['PUBLIC'];
-                        $aTempPage['status_uiicon'] = 'unhide';
-                        break;
-
-                    case 'private':
-                        $aTempPage['status_icon'] = "private_16.png";
-                        $aTempPage['status_text'] = $TEXT['PRIVATE'];
-                        $aTempPage['status_uiicon'] = 'user';
-                        break;
-
-                    case 'registered':
-                        $aTempPage['status_icon'] = "keys_16.png";
-                        $aTempPage['status_text'] = $TEXT['REGISTERED'];
-                        $aTempPage['status_uiicon'] = 'sign in';
-                        break;
-
-                    case 'hidden':
-                        $aTempPage['status_icon'] = "hidden_16.png";
-                        $aTempPage['status_text'] = $TEXT['HIDDEN'];
-                        $aTempPage['status_uiicon'] = 'hide';
-                        break;
-
-                    case 'none':
-                        $aTempPage['status_icon'] = "none_16.png";
-                        $aTempPage['status_text'] = $TEXT['NONE'];
-                        $aTempPage['status_uiicon'] = 'lock';
-                        break;
-
-                    case 'deleted':
-                        $aTempPage['status_icon'] = "deleted_16.png";
-                        $aTempPage['status_text'] = $TEXT['DELETED'];
-                        $aTempPage['status_uiicon'] = 'recycle red';
-                        break;
-
-                    default:
-                        die(LEPTON_tools::display("Error: [20012] ".$aTempPage['visibility']. " unknown!", "pre", "ui message red"));
-                        break;
-
-                }
-*/
                 $aTempPage['subpages'] = [];
-                $this->make_list($aTempPage['page_id'], $aTempPage['subpages']);
+                $this->makeList($aTempPage['page_id'], $aTempPage['subpages']);
 
                 if (isset($aTempPage['link']))
                 {
