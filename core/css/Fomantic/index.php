24e56ddeaea45a49fbe33e515410242aabcd35d6
aldus <drp@cms-lab.com>
2026-06-13 00:27:29 +0200

    Test mit css[4]

--- core/css/Fomantic/index.php
+++ core/css/Fomantic/index.php
@@ -1,31 +1,23 @@
 <?php
 
 /**
- *  @module      	Library Fomantic
- *  @version        see info.php of this module
- *  @author         LEPTON project
- *	@copyright      2010-2026 LEPTON Project
- *  @license        https://opensource.org/licenses/MIT
- *  @license terms  see info.php of this addon
- *  @platform       see info.php of this addon
+ * @package         Subway
+ * @version         0.1.0
+ * @authors         Kant (Aldus)
+ * @license         CC BY-SA 4.0
+ * @license_terms   https://creativecommons.org/licenses/by-sa/4.0/
+ * @platform        WBCE 1.6.x
+ * @requirements    PHP 8.4.x (8.3 recommented)
  */
 
-// include secure.php to protect this file and the whole CMS!
-if(!defined("SEC_FILE")){define("SEC_FILE",'/framework/secure.php' );}
-if (defined('LEPTON_PATH')) {	
-	include LEPTON_PATH.SEC_FILE;
-} else {
-	$oneback = "../";
-	$root = $oneback;
-	$level = 1;
-	while (($level < 10) && (!file_exists($root.SEC_FILE))) {
-		$root .= $oneback;
-		$level += 1;
-	}
-	if (file_exists($root.SEC_FILE)) { 
-		include $root.SEC_FILE;   
-	} else {
-		trigger_error(sprintf("[ <b>%s</b> ] Can't include secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
-	}
+$base = "../../config.php";
+$c = 0;
+while((!file_exists($base)) || (++$c < 10))
+{
+    $base = "../".$base;
 }
-// end include secure file
+$lookup = str_replace("config.php", "index.php", $base);
+if (file_exists($lookup))
+{
+    header("Location: ".$lookup, true, 301);
+}
